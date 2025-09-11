a733a017a5a43b8bfe0465b0dada77e7edf086f3
Luca Diegel <luca@trenz.de>
2025-09-11 12:28:09 +0200

    added new project for abstractions and services

---
+++ UmaEventReader.Services/ImageTextExtractor.cs
@@ -0,0 +1,15 @@
+using System.Drawing;
+using Tesseract;
+
+namespace UmaEventReader.Services;
+
+public class ImageTextExtractor
+{
+    // todo add TextExtractResult with mean confidence?
+    // public Task<string> ExtractAsync(Bitmap bmpImage)
+    // {
+
+    // }
+
+    // private TesseractEngine
+}
